1ff0cf182eb26a4cc7c12a2b75176c546f9accd1
Matthew Cranford <mcranfordmail@gmail.com>
2018-07-10 13:34:29 -0600

    Add url test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,13 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('has url', function() {
+            for(feed of allFeeds) {
+                console.log(feed.url);
+                expect(feed.url).not.toBe(undefined)
+                expect(feed.url).not.toBe('')
+            }
+        })
 
 
         /* TODO: Write a test that loops through each feed
